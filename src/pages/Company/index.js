31d7418e304db4a6209cf32c01ab1bfa1fb27de0
Abdul-Hanann <124266356+Abdul-Hanann@users.noreply.github.com>
2023-07-09 15:44:45 +0500

    company cleaned

--- src/pages/Company/index.js
+++ src/pages/Company/index.js
@@ -1,101 +1,25 @@
-import React, { useEffect, useMemo, useState } from "react"
+import React, { useState } from "react"
 import { Link } from "react-router-dom"
-import { isEmpty } from "lodash"
 import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import TableContainer from "./TableContainer"
-import * as Yup from "yup"
-import { useFormik } from "formik"
-// import { jobWbsData } from "common/data/jobWbsData"
 
 //import components
 import Breadcrumbs from "components/Common/Breadcrumb"
 import DeleteModal from "components/Common/DeleteModal"
-import DatePicker from "react-datepicker"
-import "react-datepicker/dist/react-datepicker.css"
 
-import {
-  addNewJobList as onAddNewJobList,
-  updateJobList as onUpdateJobList,
-  deleteJobList as onDeleteJobList,
-  fetchJobList,
-} from "store/actions"
+import { deleteJobList as onDeleteJobList } from "store/actions"
 
 //redux
-import { useSelector, useDispatch } from "react-redux"
-
-import LazyLoadImage from "components/Common/LazyLoadImage"
-import Dropzone from "react-dropzone"
-import {
-  Col,
-  Row,
-  UncontrolledTooltip,
-  Modal,
-  ModalHeader,
-  ModalBody,
-  Form,
-  Input,
-  FormFeedback,
-  Label,
-  Card,
-  CardBody,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-} from "reactstrap"
+import { useDispatch } from "react-redux"
+import { Col, Row, Card, CardBody } from "reactstrap"
 
 function Company() {
   //meta title
   document.title = "Job Wbs | SAIT Job Board"
 
   const [modal, setModal] = useState(false)
-  const [isEdit, setIsEdit] = useState(false)
-
-  const [jobsList, setJobsList] = useState([])
   const [job, setJob] = useState(null)
   const dispatch = useDispatch()
-  // const { jobs } = useSelector(state => state.JobListReducer)
-
-  // validation
-  const validation = useFormik({
-    // enableReinitialize : use this flag when initial values needs to be changed
-    enableReinitialize: true,
-
-    initialValues: {
-      name: (job && job.name) || "",
-      tasks: (job && job.tasks) || "",
-      id: (job && job.id) || "",
-    },
-    validationSchema: Yup.object({
-      name: Yup.string().required("Please Enter Your Job Name"),
-      tasks: Yup.string().required("Please Enter Your Job Date"),
-      id: Yup.string().required("Please Enter Your Job Site ID"),
-    }),
-    onSubmit: values => {
-      if (isEdit) {
-        const updateJobList = {
-          id: job ? job.id : 0,
-          name: values.name,
-          tasks: values.tasks,
-        }
-        // update Job
-        // dispatch(onUpdateJobList(updateJobList))
-        validation.resetForm()
-      } else {
-        const newJobList = {
-          id: Math.floor(Math.random() * (30 - 20)) + 20,
-          // JobNo: values["JobNo"],
-          name: values["name"],
-          tasks: values["tasks"],
-          id: values["id"],
-        }
-        // save new Job
-        // dispatch(onAddNewJobList(newJobList))
-        validation.resetForm()
-      }
-      toggle()
-    },
-  })
 
   const toggle = () => {
     if (modal) {
@@ -108,11 +32,6 @@ function Company() {
 
   //delete Job
   const [deleteModal, setDeleteModal] = useState(false)
-
-  const onClickDelete = job => {
-    setJob(job)
-    setDeleteModal(true)
-  }
 
   const handleDeletejob = () => {
     if (job && job.id) {
